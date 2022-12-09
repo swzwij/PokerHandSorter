caa35c1bbae4040d97da8ffe6275c5e89d8f76cd
Samuel <70766878+swzwij@users.noreply.github.com>
2022-12-09 14:43:30 +0100

    now buildable with android apk

--- Assets/HandRandomIzer.cs
+++ Assets/HandRandomIzer.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Progress;
 
 public class HandRandomIzer : MonoBehaviour
 {
